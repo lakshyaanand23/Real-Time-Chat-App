36f6bfb4c1c35cfd47eac1fbbce407c2894bdb68
Lakshyaanand23 <lakshyaanand2323@gmail.com>
2025-11-15 09:47:24 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -48,9 +48,14 @@ io.on("connection", (socket) => {
 // Connect to MongoDB
 await connectDB();
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
 
+if(process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 5000;
+  server.listen(PORT, () => console.log("Server is running on PORT: " + PORT));
+}
+
+//Export server for vercel deployment
+export default server;
 
 // import express from "express";
 // import cors from "cors";
